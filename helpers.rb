09e35bfd2eb067e1abf43afb3047ee0dd046e937
Muriel Picone Farías <m.picone.farias@mpiconefarias.local>
2021-12-20 11:33:25 +0100

    Update helpers print format

--- helpers.rb
+++ helpers.rb
@@ -1,8 +1,8 @@
 def print_test_message(res, expected_res)
   if res == expected_res
-    puts "Yay! Test passed with expected result #{res}."
+    puts "Yay! Test passed with expected result: \n#{res}"
   else
-    puts "Try again! Test failed with result #{res}, but expected #{expected_res}."
+    puts "Try again! Test failed with result: \n#{res} \nExpected: \n#{expected_res}"
   end
 end
 
@@ -17,7 +17,7 @@ def run_for_real(method_to_run)
   puts 'Running on given input...'
   input = read_input('input.txt')
   res = method(method_to_run).call(input)
-  puts "Result: #{res}."
+  puts "Result: \n#{res}"
 end
 
 def run(part, part_method, expected_test_res)
